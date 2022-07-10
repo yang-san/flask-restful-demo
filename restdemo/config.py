bf61060ca4e7a92bab6a4e92d160dad8ba6934db
david <yangdavid0614@gmail.com>
2022-07-11 01:46:13 +0800

    配置文件設置

---
+++ restdemo/config.py
@@ -0,0 +1,23 @@
+'''
+配置文件
+'''
+class DB_settings:
+    def __init__(self):
+        DB_TYPE = "mysql"
+        DB_DRIVER = "pymysql"
+        DB_USERNAME = "root"
+        DB_PASSWORD = "root"
+        DB_HOST = "localhost"
+        DB_PORT = "3306"
+        DB_NAME = "flask_demo"
+
+        #DB_URL = "sqlite:///demo.db"
+        self.DB_URL = f"{DB_TYPE}+{DB_DRIVER}://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    
+    def get_db(self):
+        return self.DB_URL
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = DB_settings().get_db()
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
